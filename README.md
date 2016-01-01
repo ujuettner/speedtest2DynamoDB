3db2e294e0008b226ea2861a24724d7875ddece1
Udo Juettner <udo.juettner@gmail.com>
2016-01-01 02:48:35 +0100

    Added a small script to export the DynamoDB table to CSV.

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 `speedtest2dynamodb.py` runs [speedtest-cli](https://github.com/sivel/speedtest-cli), parses its output and writes the values to Amazon DynamoDB.
 
-It uses [Boto3](https://github.com/boto/boto3). Therefore, it relies on a properly set up `~/.aws/credentials` and `~/.aws/config`, respectively.
+`dynamodb2csv.py` scans an Amazon DynamoDB table and prints out the items in CSV format.
+
+Both use [Boto3](https://github.com/boto/boto3). Therefore, it relies on a properly set up `~/.aws/credentials` and `~/.aws/config`, respectively.
---
+++ dynamodb2csv.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Exports Amazon DynamoDB table items to CSV."""
+
+from __future__ import print_function
+import boto3
+import datetime
+
+
+_DYNAMODB_TABLE_NAME = 'speedtest'
+_FIELDS = [
+    'timestamp',
+    'ping_ms',
+    'download_bit_per_second',
+    'upload_bit_per_second'
+]
+_FIELD_SEPARATOR = ','
+
+
+def main():
+    """The main entry point."""
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table(_DYNAMODB_TABLE_NAME)
+    response = table.scan()
+    print(_FIELD_SEPARATOR.join(_FIELDS))
+    for item in response['Items']:
+        record_items = list()
+        for field in _FIELDS:
+            if field == 'timestamp':
+                record_items.append(
+                    datetime.datetime.fromtimestamp(
+                        item[field]
+                    ).strftime('%Y-%m-%d %H:%M:%S')
+                )
+            else:
+                record_items.append(str(item[field]))
+        print(_FIELD_SEPARATOR.join(record_items))
+
+if __name__ == '__main__':
+    main()
+
+# vim:ts=4:sw=4:expandtab
